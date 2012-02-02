ad87588078e467ac86d35f8dcbef919a5b5d7bec
Ricardo L. Dani <ricardodani@gmail.com>
2012-02-02 18:00:10 -0200

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*- 
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='m2mikp',
+    version='dev',
+    description='ManyToManyField ImageKit Plugin',
+    author='Ricardo Dani',
+    author_email='ricardodani@gmail.com',
+    url='https://github.com/ricardodani/M2MIKP',
+    packages=['m2mikp'],
+)
